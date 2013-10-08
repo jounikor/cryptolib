a470920da9ae9c4089c05d633d9709b7a765d06c
Jouni Korhonen <jouni.korhonen@renesasmobile.com>
2013-10-08 10:08:32 +0300

    Added synchronization.h
    Signed-off-by: Jouni Korhonen <jouni.korhonen@renesasmobile.com>

---
+++ synchronization.h
@@ -0,0 +1,19 @@
+#ifndef _synchronization_h_included
+#define _synchronization_h_included
+
+/**
+ * \file synchronization.h
+ * \brief Stubs for future synchronization primitives.
+ *
+ */
+
+
+#define ATOMIC_OPERATION(x) (x)
+
+#define ENTER_CRITICAL_SECTION  {}
+#define LEAVE_CRITICAL_SECTION  {}
+
+
+
+#endif /* _synchronization_h_included */
+
